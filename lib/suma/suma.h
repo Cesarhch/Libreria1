464dc7fafe00a7bc00cd4586018692328823fd28
Cesarhch <cesarhch1@gmail.com>
2023-02-10 15:48:01 +0100

    first commit

---
+++ lib/suma/suma.h
@@ -0,0 +1,10 @@
+#ifndef suma_h
+#define suma_h
+
+#include "Arduino.h"
+
+void texto(){
+    Serial.println("Esto esta dentro de la funcion suma");
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <suma.h>
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("");
+  Serial.println("Hola");
+  texto();
+  }
+
+void loop() {
+  
+  }
